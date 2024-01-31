adbaf5972cd6df21e69568483f4fefeb28bd7175
Саня <70769398+Deadend4@users.noreply.github.com>
2024-01-31 18:10:22 +0300

    add x

--- src/App.js
+++ src/App.js
@@ -1,16 +1,44 @@
-import logo from './logo.svg';
 import './App.css';
+import {Helmet} from 'react-helmet'
 import { useState } from 'react';
-
-export function Square() {
-  return <button className="square">X</button>
-}
 
 export default function App() {
   return (
-    <div>
-      <h1>Tic-Tac-Toe</h1>
-      <Square/>
-    </div>
+      <div>
+        <Helmet>
+          <title>Tic-Tac-Toe</title>
+          <meta name="description" content="Tic-tac-toe on React" />
+        </Helmet>
+        <Board/>
+      </div>
   );
+}
+function Square() {
+  const [value, setValue] = useState(null);
+  function handleClick() {
+    setValue('X');
+  }
+  return <button className="square" onClick={handleClick}>{value}</button>;
+}
+function Board() {
+  return (
+  <>
+    <div className="board-row">
+      <Square value="1"/>
+      <Square value="2"/>
+      <Square value="3"/>
+    </div>
+    <div className="board-row">
+      <Square value="4"/>
+      <Square value="5"/>
+      <Square value="6"/>
+    </div>
+    <div className="board-row">
+      <Square value="7"/>
+      <Square value="8"/>
+      <Square value="9"/>
+      </div>
+  </>
+  
+  )
 }
